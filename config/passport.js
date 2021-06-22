2551ce570fc5069cd3089a5a3f9f6a768ad6eac6
yattishr <yattish@gmail.com>
2021-06-22 15:21:02 +0200

    Added logic for creating new user.

--- config/passport.js
+++ config/passport.js
@@ -9,6 +9,27 @@ module.exports = function(passport) {
         callbackURL: '/auth/google/callback'
     }, async(accessToken, refreshToken, profile, done) => {
         console.log('The current user profile is: ', profile)
+        const newUser = {
+            googleId: profile.id,
+            displayName: profile.displayName,
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            image: profile.photos[0].value
+        }
+
+        try {
+            let user = await User.findOne({ googleId: profile.id })
+            if(user) {
+                done(null, user) // if user is found, then continue with no error.
+            } else {
+                user = await User.create(newUser) // create a new User.
+                done(null, user)
+            }
+        } catch(err) {
+            console.error('Oooops, there was an error logging in!', err)
+        }
+
+
     }))
 
     passport.serializeUser(function(user, done) {
