357529dcd8e9ed37097fc8a6668df7a82a78883f
yattishr <yattish@gmail.com>
2021-06-24 17:09:18 +0200

    Added CRUD logic for editing and deleting User stories.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const morgan = require('morgan')
 const exphbs = require('express-handlebars');
 const passport = require('passport');
 const session = require('express-session');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
 const MongoStore = require('connect-mongodb-session')(session);
 
 const connectDB = require('./config/db')
@@ -25,17 +27,31 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+// Method Over-ride.
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
 // Appliction Logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
 // Handlebar Helpers
-const { formatDate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
 
 // Handlebars
 app.engine('.hbs', exphbs({ helpers: {
     formatDate,
+    stripTags, 
+    truncate,
+    editIcon,
+    select
 }, 
 defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
@@ -57,6 +73,12 @@ app.use(session({
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global variable
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 
 
 // Static Assets folder
